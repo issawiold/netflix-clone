40d01e8b22795e23c88054df59b81aa59fab0431
issawiold <omar.m.issawi.95@gmail.com>
2023-07-16 01:29:32 +0000

    nf-clone

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,42 @@
+import "./Home.css"
+import {Routes,Route} from "react-router-dom";
+import MovieList from "../MovieList/MovieList";
+import Movie from "../Movie/Movie";
+import { useEffect,useState } from "react";
+function Home() {
+    const [data,setData]=useState([])
+    async function getData(){
+        let url=process.env.REACT_APP_BACKEND_URL
+        const response = await fetch(`${url}/trending`);
+        const movies = await response.json(); 
+        //e.id, e.title, e.release_date, e.poster_path, e.overview
+        setData(movies)
+    }
+    useEffect(()=>{getData()},[])
+    const [id,setId]=useState("123")
+    const  getValue=(value)=>{
+      return setId(value)
+    }
+    let obj=data.filter(e=>e.id===id )
+      return (
+        <section>
+            <nav className="nav1">
+         <h1>NetBlex</h1>
+         <h3>for copyrights reasons</h3>
+         </nav>
+         <main>
+          <Routes>
+          <Route>
+          <Route path="/" element={data.map( e=><  MovieList id={e.id} title={e.title} date={e.release_date} img={e.poster_path} callback={getValue}/> ) }/>
+          <Route path="/movie/:id" element={ < Movie id={obj[0].id} title={obj[0].title} date={obj[0].release_date} img={obj[0].poster_path} overview={obj[0].overview}/>} />
+          </Route>
+          </Routes>
+          </main>
+          <nav className="nav2">
+         <h3>Join us now</h3>
+         <h3>Exclusive movies and tv shows</h3>
+         </nav>
+        </section>
+      );
+    }
+    export default Home
---
+++ src/Components/Movie/Movie.js
@@ -0,0 +1,19 @@
+import Card from 'react-bootstrap/Card';
+
+function Movie(props) {
+  return (
+    
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={props.img} alt={props.title}/>
+      <Card.Body>
+      <Card.Title>{props.title}</Card.Title>
+        <Card.Text>
+          release date:{props.date}
+          overview : {props.overview}  
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Movie;
